a0ca7a3d5fd26ad4974cd3a37caa7ce9566a9d05
multiscripter <ILL-JAH@yandex.ru>
2020-09-02 13:19:58 +0300

    Added tree taxonomy menu.

--- djbook/views.py
+++ djbook/views.py
@@ -12,7 +12,7 @@ class Node:
 
 def get_kids(node):
     nodes = []
-    kids = Taxonomy.objects.filter(parent_id=node.id)
+    kids = Taxonomy.objects.filter(parent_id=node.id).order_by('eng_word')
     if kids:
         for kid in kids:
             child = Node(kid, node)
@@ -36,12 +36,14 @@ def build_tree(tax_id):
 def build_html_by_tree(tree):
     html = ''
     if tree.kids:
-        html += '<div>' + tree.obj.rus_word + '</div>\n'
-        html += '<ul>\n'
-        # TODO: добавить вывод дочерних таксономий итеративно.
+        html += '<ul class="list-group list-group-compact">\n'
         for kid in tree.kids:
-            html += '   <li>'
-            html += kid.obj.rus_word
+            html += '   <li class="list-group-item list-group-item-action">'
+            elem = '<div><b>' + kid.obj.eng_word + '</b> &ndash;&nbsp;'
+            elem += kid.obj.rus_word + '</div>\n'
+            if kid.kids:
+                elem += build_html_by_tree(kid)
+            html += elem
             html += '</li>\n'
         html += '</ul>\n'
     return html
@@ -50,15 +52,25 @@ def build_html_by_tree(tree):
 def home(request):
     # Получить все записи с полем parent = null
     # tax_list = Taxonomy.objects.filter(parent__isnull=True)
-    tree = build_tree(17)
-    tree_html = ''
+
+    tree = build_tree(15)
+    elem = '<h6 class="mt-3 list-group-head">'
+    elem += '<b>' + tree.obj.eng_word + '</b> &ndash;&nbsp;'
+    elem += tree.obj.rus_word + '</h6>\n'
+    tax_most_tree_html = elem
     if tree:
-        tree_html = build_html_by_tree(tree)
-    tax_most_list = Taxonomy.objects.filter(parent_id=15)
-    tax_theme_list = Taxonomy.objects.filter(parent_id=17).order_by('eng_word')
+        tax_most_tree_html += build_html_by_tree(tree)
+
+    tree = build_tree(17)
+    elem = '<h6 class="mt-3 list-group-head">'
+    elem += '<b>' + tree.obj.eng_word + '</b> &ndash;&nbsp;'
+    elem += tree.obj.rus_word + '</h6>\n'
+    tax_theme_tree_html = elem
+    if tree:
+        tax_theme_tree_html += build_html_by_tree(tree)
+
     data = {
-        'tax_most_list': tax_most_list,
-        'tax_theme_list': tax_theme_list,
-        'tree_html': tree_html
+        'tax_most_tree_html': tax_most_tree_html,
+        'tax_theme_tree_html': tax_theme_tree_html
     }
     return render(request, 'djbook/home.html', data)
