1ac6e6d1ebc2f252b3ac99fc30c07a85e93081b8
multiscripter <ILL-JAH@yandex.ru>
2020-09-04 23:11:25 +0300

    Tests fixed.

--- djbook/tests.py
+++ djbook/tests.py
@@ -1,12 +1,20 @@
-from unittest import skip
-
 from django.test import TestCase
 from djbook.views import build_tree
 from djbook.views import build_html_by_tree
+from unittest import skip
 from .models import Taxonomy
 
 
 class ViewsTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Действия перед всеми тестами.
+        При переопределении setUpClass(cls) всегда нужно вызывать
+        метод super(%имя_текущего_класса_тестов%, cls).setUpClass()"""
+        super(ViewsTest, cls).setUpClass()
+
+        cls.tax_set = Taxonomy.objects.all().order_by('eng_word')
+
     def setUp(self):
         data = [
             ['By themes', 'По темам', None, 'themes'],
@@ -30,12 +38,18 @@ class ViewsTest(TestCase):
     @skip("Don't want to test")
     def test_build_tree(self):
         """Tests None build_tree(tax_id)"""
-        tree = build_tree(1)
+        tree = build_tree(self.tax_set, 1)
         self.assertTrue(len(tree.kids))
 
     def test_build_html_by_tree(self):
         """Tests String build_html_by_tree(tree)"""
-        tree = build_tree(1)
+        tree = build_tree(self.tax_set, 1)
         tree_html = build_html_by_tree(tree)
-        # print(tree_html)
         self.assertTrue(tree_html)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Действия после всех тестов.
+        При переопределении tearDownClass(cls) всегда нужно вызывать
+        метод super(%имя_текущего_класса_тестов%, cls).tearDownClass()"""
+        super(ViewsTest, cls).tearDownClass()
